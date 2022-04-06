f09b1431c8f57f12ce0f71e0cd1286224ad37955
chrisob55 <4126049+chrisob55@users.noreply.github.com>
2022-04-06 11:58:31 +0100

    configuring the step

--- step.sh
+++ step.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+start_workflow() {
+    STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" \
+        https://app.bitrise.io/app/$BITRISE_APP_SLUG/build/start.json \
+        --data '{"hook_info":{"type":"bitrise","build_trigger_token":"'"$BITRISE_BUILD_TRIGGER_TOKEN"'"},"build_params":{"branch":"'"$BITRISE_GIT_BRANCH"'","commit_hash":"'"$BITRISE_GIT_COMMIT"'","workflow_id":"'"$1"'"},"triggered_by":"curl"}' \
+    )
+    if (($STATUSCODE >= 200 && $STATUSCODE < 300)); then
+        echo "Started the workflow: ${1}"
+    else
+        echo "Failed to start workflow: ${1}"
+        exit 1
+    fi
+}
 
-#
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+pending_health_check() {
+    # setup health check with Github API
+    STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" \
+        -X POST -H 'Accept: application/vnd.github.v3+json' \
+        -H 'Authorization: token '"$GITHUB_TOKEN"'' https://api.github.com/repos/hmrc/$REPO_NAME/statuses/$BITRISE_GIT_COMMIT \
+        -d '{"state":"pending", "target_url": "https://app.bitrise.io/build/'"$1"'", "description": "'"$BITRISEIO_GIT_REPOSITORY_SLUG"'", "context": "ci/bitrise/'"$BITRISE_GIT_BRANCH"'/'"$1"'"}' \
+    )
+    if (($STATUSCODE >= 200 && $STATUSCODE < 300)); then
+        echo "Updated status for workflow: ${1}"
+    else
+        echo "Failed to update status for workflow: ${1}"
+        exit 1
+    fi
+}
 
-#
-# --- Exit codes:
-# The exit code of your Step is very important. If you return
-#  with a 0 exit code `bitrise` will register your Step as "successful".
-# Any non zero exit code will be registered as "failed" by `bitrise`.
+FLOWS=(${step_workflows// / })
+
+for i in "${FLOWS[@]}"
+do
+    start_workflow $i
+    
+    pending_health_check $i
+done
